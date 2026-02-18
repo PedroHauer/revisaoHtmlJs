fe2a6af999e29d1b7a1fd349f0915972485d1c49
Pedro Hauer <pedrohhcg@gmail.com>
2026-02-18 20:06:11 -0300

    subindo o projeto

---
+++ exercicios/ex01.js
@@ -0,0 +1,7 @@
+// Escreva um programa que declare duas variáveis, “nome” e “idade”, e as imprima no console em uma frase que diga “Olá, meu nome é [nome] e eu tenho [idade] anos”.
+// exercicio 1
+let nome = "João";
+let idade = 30; 
+console.log(`Olá, meu nome é ${nome} e eu tenho ${idade} anos.`);
+
+
